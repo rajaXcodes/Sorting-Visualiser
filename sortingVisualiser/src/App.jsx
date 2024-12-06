4c349aeae5d801743448083bdcb86be671e641a5
rajaXcodes <konig.9122005@gmail.com>
2024-12-06 20:50:31 +0530

    refactored the insertion

--- sortingVisualiser/src/App.jsx
+++ sortingVisualiser/src/App.jsx
@@ -4,24 +4,26 @@ import BubbleSort from "./components/bubblesort/bubbleSort";
 import InsertionSort from "./components/insertionsort";
 import MergeSort from "./components/mergesort";
 import QuickSort from "./components/quicksort";
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 function App() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Sorting Visualiser</h1>
-     <div >
-      <button onClick={()=>navigate('/')}>Bubble Sort</button>
-      <button onClick={()=>navigate('/quicksort')}>Quick Sort</button>
-      <button onClick={()=>navigate('/insertionsort')}>Insertion Sort</button>
-      <button onClick={()=>navigate('/mergesort')}>Merge Sort</button>
-     </div>
+      <div>
+        <button onClick={() => navigate("/")}>Bubble Sort</button>
+        <button onClick={() => navigate("/quicksort")}>Quick Sort</button>
+        <button onClick={() => navigate("/insertionsort")}>
+          Insertion Sort
+        </button>
+        <button onClick={() => navigate("/mergesort")}>Merge Sort</button>
+      </div>
 
       <Routes>
-        <Route path="/" element={<BubbleSort  />} />
-        <Route path="/insertionsort" element={<InsertionSort key="merge"/>} />
+        <Route path="/" element={<BubbleSort />} />
+        <Route path="/insertionsort" element={<InsertionSort key="insert" />} />
         <Route path="/mergesort" element={<MergeSort key="merge" />} />
-        <Route path="/quicksort" element={<QuickSort key="quick"/>} />
+        <Route path="/quicksort" element={<QuickSort key="quick" />} />
       </Routes>
     </div>
   );
